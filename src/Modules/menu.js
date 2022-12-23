93149379acbc95e4bd5fe032c83176be1ba9f022
vimalsonara <vimalsonara35@gmail.com>
2022-12-23 19:59:25 +0530

    multiple div logic corrected

--- src/Modules/menu.js
+++ src/Modules/menu.js
@@ -2,12 +2,13 @@ export function menu() {
     const container = document.querySelector('#content');
 
     const menuItem = ['Margherita Pizza', 'Italian Pizza', 'Masala Maggie', 'Mexixan Sizzler', 'Aloo Paratha', 'Nutella Shake'];
+    const div = document.createElement('div');
+    div.classList.add('menus')
+    const ul = document.createElement('ul')
 
     menuItem.forEach(createMenu);
 
     function createMenu(item) {
-        const div = document.createElement('div');
-        const ul = document.createElement('ul')
         const li = document.createElement('li');
         container.appendChild(div);
         div.appendChild(ul);
