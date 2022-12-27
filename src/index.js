6233d6d6d84c9f263a216e7f48bcdad058ab479c
vimalsonara <vvsonara95@gmail.com>
2022-12-27 19:49:16 +0530

    tabSwitch functionality added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import {header} from './Modules/header';
 import {bannerImg} from './Modules/banner';
 import {menu} from './Modules/menu';
+import {contactUs} from './Modules/contact';
 import './styles.css';
 
 header();
 bannerImg();
-menu();
+
+const navBar = document.querySelector('nav');
+navBar.addEventListener('click', e => {
+    if(e.target.classList.contains('home')) {
+        const page = document.getElementById('page');
+        page.remove();
+        bannerImg();
+    } else if(e.target.classList.contains('menu')) {
+        const page = document.getElementById('page');
+        page.remove();
+        menu();
+    } else {
+        const page = document.getElementById('page');
+        page.remove();
+        contactUs();
+    }
+});
